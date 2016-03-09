9349b7b32d29274a843b79dd927c445a918687cc
209255 <209255@student.pwr.edu.pl>
2016-03-09 16:59:02 +0100

    Add UnitTests

---
+++ GameOfLifeTests/GameOfLifeUnitTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLifeTests
+{
+    [TestClass]
+    public class GameOfLifeUnitTests
+    {
+        [TestMethod]
+        public void ResurectionCausedThreeNeighbor()
+        {
+            var game = new GameOfLife.GameOfLife("Resurection.txt");
+            game.NextAge();
+            Assert.AreEqual(game.IsDeadOrAlive(1, 1), true);
+        }
+
+        [TestMethod]
+        public void DeathCausedMoreThanThreeNeighbor()
+        {
+            var game = new GameOfLife.GameOfLife("Death4Nei.txt");
+            game.NextAge();
+            Assert.AreEqual(game.IsDeadOrAlive(1, 1), false);
+        }
+
+        [TestMethod]
+        public void DeathCausedLessThanTwoNeighbor()
+        {
+            var game = new GameOfLife.GameOfLife("Death1Nei.txt");
+            game.NextAge();
+            Assert.AreEqual(game.IsDeadOrAlive(1, 1), false);
+        }
+
+        [TestMethod]
+        public void StayedAliveCausedThreeNeighbor()
+        {
+            var game = new GameOfLife.GameOfLife("Alive3Nei.txt");
+            game.NextAge();
+            Assert.AreEqual(game.IsDeadOrAlive(1, 1), true);
+        }
+
+        [TestMethod]
+        public void StayedAliveCausedTwoNeighbor()
+        {
+            var game = new GameOfLife.GameOfLife("Alive2Nei.txt");
+            game.NextAge();
+            Assert.AreEqual(game.IsDeadOrAlive(1, 1), true);
+        }
+
+        [TestMethod]
+        public void StayedDeadCausedMoreThanThreeNeighbor()
+        {
+            var game = new GameOfLife.GameOfLife("Death5Nei.txt");
+            game.NextAge();
+            Assert.AreEqual(game.IsDeadOrAlive(1, 1), false);
+        }
+
+        [TestMethod]
+        public void StayedDeadCausedLessThanThreeNeighbor()
+        {
+            var game = new GameOfLife.GameOfLife("Death2Nei.txt");
+            game.NextAge();
+            Assert.AreEqual(game.IsDeadOrAlive(1, 1), false);
+        }
+    }
+}
